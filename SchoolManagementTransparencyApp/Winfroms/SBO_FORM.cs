b0093cfd822d8a7aae5b53f57e327d5550f98e07
turtoga123455@gmail.com <vidsrandom143@gmail.com>
2026-04-10 14:51:39 +0800

    implementing project structure folder.

---
+++ SchoolManagementTransparencyApp/Winfroms/SBO_FORM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace School_Management_Transparency.SchoolManagementTransparencyApp.Winfroms
+{
+    public partial class SBO_FORM : Form
+    {
+        public SBO_FORM()
+        {
+            InitializeComponent();
+        }
+    }
+}
