bf399cd1c2a3e4d2f217644575975fa9f0f4de1d
Jun Romulo Jarina <turtoga123455@gmail.com>
2026-04-23 19:21:16 +0800

    Enhance AdminForm UI and update resources
    Added drag control, gradient panels, and label to AdminForm. Increased form sizes for AdminForm and StudentForm. Added AdminForm.resx and updated project resources and binaries accordingly.

--- SchoolManagementTransparencyApp/Winforms/AdminForm.Designer.cs
+++ SchoolManagementTransparencyApp/Winforms/AdminForm.Designer.cs
@@ -29,11 +29,76 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.contentContainerPanel = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contentContainerPanel.SuspendLayout();
+            this.guna2GradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this.contentContainerPanel;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // contentContainerPanel
+            // 
+            this.contentContainerPanel.BackgroundImage = global::School_Management_Transparency.Properties.Resources.ncbii_image_2;
+            this.contentContainerPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.contentContainerPanel.Controls.Add(this.guna2GradientPanel1);
+            this.contentContainerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contentContainerPanel.Location = new System.Drawing.Point(0, 0);
+            this.contentContainerPanel.Name = "contentContainerPanel";
+            this.contentContainerPanel.Size = new System.Drawing.Size(1924, 918);
+            this.contentContainerPanel.TabIndex = 0;
+            // 
+            // guna2GradientPanel1
+            // 
+            this.guna2GradientPanel1.Controls.Add(this.label1);
+            this.guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2GradientPanel1.FillColor = System.Drawing.Color.Salmon;
+            this.guna2GradientPanel1.FillColor2 = System.Drawing.Color.Firebrick;
+            this.guna2GradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2GradientPanel1.Name = "guna2GradientPanel1";
+            this.guna2GradientPanel1.Size = new System.Drawing.Size(368, 918);
+            this.guna2GradientPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(25, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ADMIN DASHBOARD";
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1924, 918);
+            this.Controls.Add(this.contentContainerPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AdminForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin_Form";
+            this.contentContainerPanel.ResumeLayout(false);
+            this.guna2GradientPanel1.ResumeLayout(false);
+            this.guna2GradientPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2GradientPanel contentContainerPanel;
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private System.Windows.Forms.Label label1;
     }
 }
